724ebecb4599f8b954f5fb8dc7bccaa88df18d3e
ninjinkun <ninjin@mac.com>
2019-12-23 00:26:43 +0900

    feature: Add application components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="column">
+      <h2>1. Immutableに全Itemを更新（遅い）</h2>
+      <table class="table">
+        <tr v-for="(itemArray, index) in chunk(immutableItems, 10)" :key="index">
+          <td v-for="item in itemArray" :key="item.key" class="cell">
+            <ItemCell :item="item" @check="immutableUpdate(item, $event)" />
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="column">
+      <h2>2. 直接1アイテムのみを更新（速い）</h2>
+      <table class="table">
+        <tr v-for="(itemArray, index) in chunk(mutableItems, 10)" :key="index">
+          <td v-for="item in itemArray" :key="item.key" class="cell">
+            <ItemCell :item="item" @check="mutableUpdate(item, $event)" />
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { cloneDeep, chunk } from "lodash-es";
+import ItemCell from "./components/ItemCell.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ItemCell
+  },
+  data() {
+    const items = [...Array(10000)].map((_, index) => ({
+      id: `${index}`,
+      name: `${index}`,
+      checked: false
+    }));
+    return {
+      immutableItems: cloneDeep(items),
+      mutableItems: cloneDeep(items)
+    };
+  },
+  methods: {
+    chunk,
+    immutableUpdate(item, checked) {
+      this.immutableItems = this.immutableItems.map(i =>
+        i.id === item.id ? { ...i, checked } : { ...i }
+      );
+    },
+    mutableUpdate(item, checked) {
+      item.checked = checked;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.table {
+  border-collapse: collapse;
+}
+
+.cell {
+  border: 1px solid grey;
+}
+
+.row {
+  display: flex;
+}
+
+.column {
+  width: 50%;
+  border: 1px solid grey;
+  padding: 1em;
 }
 </style>
---
+++ src/components/ItemCell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="[{ checked: item.checked }]">
+    <div class="name">{{ item.name }}</div>
+    <input type="checkbox" :value="item.checked" @change="input" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  },
+  methods: {
+    input(event) {
+      this.$emit("check", event.target.checked, this.item.id);
+    }
+  }
+};
+</script>
+
+<style>
+.checked {
+  background: green;
+}
+
+.name {
+  white-space: nowrap;
+}
+</style>
